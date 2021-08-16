6acecf2ce23d42e4e85a73f0257beb2ae49a3485
Andre Linces <andrelinces@yahoo.com.br>
2021-08-16 20:17:24 -0300

    Inserindo códigos e configurações finais.

--- fraseDoDia/ViewController.swift
+++ fraseDoDia/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var fraseDoDia: UILabel!
+    
+    @IBAction func novaFrase(_ sender: Any) {
+        
+        var frases: [String] = []
+        
+        frases.append("Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos")
+        frases.append("Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo")
+        frases.append("Não é o mais forte que sobrevive, nem o mais inteligente. Quem sobrevive é o mais disposto à mudança")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
