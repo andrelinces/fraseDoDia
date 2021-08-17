5a9bc470ba31a0dee3c3d7eb32b67343df04f12e
Andre Linces <andrelinces@yahoo.com.br>
2021-08-16 21:47:18 -0300

    Finalizado!

--- fraseDoDia/ViewController.swift
+++ fraseDoDia/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var fraseDoDia: UILabel!
+    @IBOutlet weak var legendaResultado: UILabel!
+    
     
     @IBAction func novaFrase(_ sender: Any) {
+    
         
         var frases: [String] = []
         
-        frases.append("Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos")
-        frases.append("Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo")
-        frases.append("Não é o mais forte que sobrevive, nem o mais inteligente. Quem sobrevive é o mais disposto à mudança")
+        frases.append("Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos!")
+        frases.append("Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo!")
+        frases.append("Não é o mais forte que sobrevive, nem o mais inteligente. Quem sobrevive é o mais disposto à mudança.")
+        
+        let numeroAleatorio = arc4random_uniform(3)
+        
+        legendaResultado.text = frases[ Int (numeroAleatorio) ]
+        
+        
     }
     
     
